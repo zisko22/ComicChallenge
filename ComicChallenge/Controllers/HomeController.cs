920bfa75410f1665a374e2d170193ef406b0d5df
zisko22 <zisko22@gmail.com>
2018-05-16 11:29:54 -0700

    Added Controllers

---
+++ ComicChallenge/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using ComicChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var comics = ComicBookManager.GetComicBooks();
+            return View(comics);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var comics = ComicBookManager.GetComicBooks();
+            var comic = comics.FirstOrDefault(p => p.ComicBookId == id);
+            return View();
+        }
+    }
+}
